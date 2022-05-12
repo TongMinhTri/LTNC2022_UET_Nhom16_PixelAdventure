1e8bb98e9a2881bfef238e272db97e25add80400
Minh Duong Quang <minhqd9112003@gmail.com>
2022-05-12 16:42:42 +0700

    update image

--- Tri_Project/Stone.h
+++ Tri_Project/Stone.h
@@ -12,7 +12,7 @@ private:
 	int y;
 	int state = 0;
 	int frame = 0;
-	SDL_Rect frame_clips[4]{};//luu 17 frame
+	SDL_Rect frame_clips[20];//luu 20 frame
 	int frame_w = w_h;
 	int frame_h = w_h;
 	int speed = 4;
@@ -20,7 +20,7 @@ private:
 	double speedy = 0.4;
 	double v_x = 0;
 	double v_y = 0;
-
+	int number_frame;
 	int x_val;
 	int y_val;
 	int limit_x;
@@ -33,19 +33,19 @@ protected:
 public:
 	Stone();
 	~Stone();
-	void init_stone(SDL_Renderer* renderer, int x, int y, int limitx, int limity, string path);
-	void stone_move(SDL_Renderer* renderer, Map& map_data);
+	void init_stone(SDL_Renderer* renderer, int x, int y, int limitx, int limity, string path,int k);
+	void stone_move(SDL_Renderer* renderer);
 	//void show(SDL_Renderer* renderer);
 	void setImg(SDL_Renderer* renderer, string path);
-	void show_frame(SDL_Renderer* renderer, string path);
+	void show_frame_up(SDL_Renderer* renderer ,double speed_run);
 	void setPos(int x, int y);//xet kich thuoc anh
 	SDL_Rect getRect_stone();
-	void set_Stone(SDL_Renderer* renderer, int a, int b, string path);
+	void set_Stand(SDL_Renderer* renderer, int a, int b, string path);
 	void set_clips();
-	void Stone_Move_Circle(SDL_Renderer* renderer, Map& map_data);
+	void Stone_Move_Circle(SDL_Renderer* renderer);
 	void DoStone_Circle();
-	void stone_move_up(SDL_Renderer* renderer, Map& map_data);
-	void show_spikes(SDL_Renderer* renderer);
+	void stone_move_up(SDL_Renderer* renderer);
+	void show_stand(SDL_Renderer* renderer);
 }
 
 #endif // !STONE_H
